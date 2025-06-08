aa323d88c163f6c338186122dac72b98ae9ff4b9
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2025-06-08 22:11:11 +0200

    cleanup

--- apps/server/env.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Env } from '@/utils/env'
-
-declare global {
-  namespace NodeJS {
-    // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-    interface ProcessEnv extends Env {}
-  }
-}
--- apps/server/src/utils/db.ts
+++
@@ -1,5 +0,0 @@
-import process from 'node:process'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-export const db = drizzle(postgres(process.env.DB_URL))
