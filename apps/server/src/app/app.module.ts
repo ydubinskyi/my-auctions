90c120411c6c683dde96af295dd8391ae6a091aa
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2025-06-14 11:18:06 +0200

    add devtools

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 import { CoreModule } from '@my-auctions/server/core';
 
 @Module({
-  imports: [CoreModule],
+  imports: [
+    CoreModule,
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
