05a27a66d45d069c88adc70863c1c12ea213d042
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2025-06-16 11:37:53 +0200

    improve health endpoint

--- libs/server/core/src/lib/health/health.controller.ts
+++ libs/server/core/src/lib/health/health.controller.ts
@@ -1,16 +1,30 @@
 import { Controller, Get } from '@nestjs/common';
-import { HealthCheck } from '@nestjs/terminus';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
+  constructor(
+    private health: HealthCheckService,
+    private db: TypeOrmHealthIndicator,
+    private disk: DiskHealthIndicator,
+    private memory: MemoryHealthIndicator
+  ) {}
+
   @Get()
   @HealthCheck()
   check() {
-    return {
-      status: 'ok',
-      info: { alive: { status: 'up' } },
-      error: {},
-      details: { alive: { status: 'up' } },
-    };
+    return this.health.check([
+      () => this.db.pingCheck('database'),
+      () =>
+        this.disk.checkStorage('storage', { path: '/', thresholdPercent: 0.5 }),
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
+      () => this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
+    ]);
   }
 }
