bcede3534e8dc54459af91a9474beb2d2dc314b2
Yaroslav Dubinskyi <slavabilaz@gmail.com>
2025-06-16 09:05:04 +0200

    remove paid devtools module

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -1,15 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DevtoolsModule } from '@nestjs/devtools-integration';
 
 import { CoreModule } from '@my-auctions/server/core';
 
 @Module({
-  imports: [
-    CoreModule,
-    DevtoolsModule.register({
-      http: process.env.NODE_ENV !== 'production',
-    }),
-  ],
+  imports: [CoreModule],
   controllers: [],
   providers: [],
 })
--- libs/server/database/src/lib/seeders/user.seeder.ts
+++ libs/server/database/src/lib/seeders/user.seeder.ts
@@ -1,7 +1,7 @@
 import { DataSource } from 'typeorm';
 import { Seeder } from 'typeorm-extension';
 
-import { User } from '@my-auctions/server/entities';
+import { UserEntity } from '@my-auctions/server/entities';
 
 const USERS_TO_ADD = [
   {
@@ -11,11 +11,11 @@ const USERS_TO_ADD = [
     password: 'admin',
     role: 'admin',
   },
-] as Array<Partial<User>>;
+] as Array<Partial<UserEntity>>;
 
 export default class UserSeeder implements Seeder {
   public async run(dataSource: DataSource) {
-    const repository = dataSource.getRepository(User);
+    const repository = dataSource.getRepository(UserEntity);
     await repository.insert(USERS_TO_ADD);
   }
 }
